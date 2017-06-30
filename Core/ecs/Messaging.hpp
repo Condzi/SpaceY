7aab6da3e517d35ed26678c7bb6b6412ca9d101a
Condzi <conradkubacki@gmail.com>
2017-06-30 15:17:30 +0200

    better message removing in messaging module

--- Core/ecs/Messaging.hpp
+++ Core/ecs/Messaging.hpp
@@ -18,8 +18,13 @@ namespace con
 	{
 		struct messageInterface_t
 		{
+			bool safeDelete = false;
 			messageID_t id = 0;
 			const size_t uniqueID;
+
+			messageInterface_t( messageID_t _id, size_t _uniqueID ) :
+				id( std::move( _id ) ), uniqueID( std::move( _uniqueID ) )
+			{}
 			virtual ~messageInterface_t() {}
 		};
 	}
@@ -34,14 +39,14 @@ namespace con
 	===============================================================================
 	*/
 	template <typename T>
-	struct message_t final :
+	struct Message final :
 		internal::messageInterface_t
 	{
 		T data;
 
-		message_t( messageID_t _id, T _data, size_t _uniqueID ) :
-			id( _id ), data( std::move( _data ) ),
-			uniqueID( std::move( _uniqueID ) )
+		Message( messageID_t _id, T _data, size_t _uniqueID ) :
+			messageInterface_t( std::move( _id ), std::move( _uniqueID ) ),
+			data( std::move( _data ) )
 		{}
 	};
 
@@ -49,7 +54,7 @@ namespace con
 	===============================================================================
 	Created by: Condzi
 		Contains messages. GetMessage return temporary reference - it'll be invalid
-		at next update, so don't store it - copy it's message_t::data.
+		when safeDelete will be marked as true.
 
 	===============================================================================
 	*/
@@ -59,31 +64,36 @@ namespace con
 		template <typename T>
 		void AddMessage( const messageID_t id, const T& data )
 		{
-			this->messages.emplace_back( std::make_unique<message_t<T>>( id, data, this->messages.size() ) );
+			this->messages.emplace_back( std::make_unique<Message<T>>( id, data, this->messages.size() ) );
 		}
 		template <typename T>
-		message_t<T>& GetUniqueMessage( const messageID_t id )
+		Message<T>* GetUniqueMessage( const messageID_t id )
 		{
 			auto result = this->findMessage( id );
 			if ( result != this->messages.end() )
-				return static_cast<message_t<T>&>( ( *result ).get() );
+				return static_cast<Message<T>*>( ( *result ).get() );
+
+			return nullptr;
 		}
 		template <typename T>
-		std::vector<message_t<T>*> GetAllMessages( const messageID_t id )
+		std::vector<Message<T>*> GetAllMessages( const messageID_t id )
 		{
-			std::vector<message_t<T>*> vec;
+			std::vector<Message<T>*> vec;
 			vec.reserve( this->messages.size() );
 
 			for ( auto& message : this->messages )
-				if ( message->id == id )
-					vec.push_back( static_cast<message_t<T>*>( ( *result ).get() ) );
+				if ( message && message->id == id )
+					vec.push_back( static_cast<Message<T>*>( ( message.get() ) ) );
 
 			return vec;
 		}
 
 		void ClearMessages()
 		{
-			this->messages.clear();
+			this->messages.erase( std::remove_if( this->messages.begin(), this->messages.end(), []( auto& msg )
+			{
+				return ( !msg ) || msg->safeDelete;
+			} ), this->messages.end() );
 		}
 
 	private:
